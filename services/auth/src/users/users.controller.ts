f24821c15aa6b5babbfd2de2ec6cbcd349b1a67f
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-04-08 19:04:57 +0200

    Remove some unused code

--- services/auth/src/users/users.controller.ts
+++ services/auth/src/users/users.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,8 +30,8 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string) {
+    return this.usersService.update(+id);
   }
 
   @Delete(':id')
