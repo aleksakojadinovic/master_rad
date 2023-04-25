fc080e9269f2d167cc0794595f4ecc5e355617a1
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-04-25 22:10:08 +0200

    Fix padding

--- services/ssr/src/components/AppWrapper.js
+++ services/ssr/src/components/AppWrapper.js
@@ -1,6 +1,7 @@
 import { wrapper } from '@/redux/store';
 import React, { Fragment } from 'react';
 import NavigationBar from './Navigation/NavigationBar';
+import { Box } from '@mui/material';
 
 function AppWrapper({ Component, pageProps }) {
   wrapper.useHydration(pageProps);
@@ -8,7 +9,9 @@ function AppWrapper({ Component, pageProps }) {
   return (
     <Fragment>
       <NavigationBar />
-      <Component {...pageProps} />
+      <Box sx={{ marginTop: '12px' }}>
+        <Component {...pageProps} />
+      </Box>
     </Fragment>
   );
 }
