32efccb97bb532adbbd286eeae26a6def54a53d3
Aleksa Kojadinovic <kojadinovic.aleksa98@gmail.com>
2023-05-04 23:29:48 +0200

    Change user chip styling

--- services/ssr/src/components/User/UserChip.js
+++ services/ssr/src/components/User/UserChip.js
@@ -1,24 +1,20 @@
 import { Avatar, Chip } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
 import React from 'react';
 
 export default function UserChip({ user: { firstName, lastName, roles } }) {
   const initials = `${firstName[0]}${lastName[0]}`;
   const fullName = `${firstName} ${lastName}`;
-  const color = roles.map((role) => role.name).includes('agent')
-    ? 'primary'
-    : 'success';
 
-  const avatarBgColor = roles.map((role) => role.name).includes('agent')
-    ? 'blue'
-    : deepOrange[500];
+  const variant = roles.map((role) => role.name).includes('agent')
+    ? 'contained'
+    : 'outlined';
 
   return (
     <Chip
       size="small"
-      color={color}
-      variant="contained"
-      avatar={<Avatar sx={{ bgcolor: avatarBgColor }}>{initials}</Avatar>}
+      color="primary"
+      variant={variant}
+      avatar={<Avatar sx={{ bgcolor: 'blue' }}>{initials}</Avatar>}
       label={fullName}
     />
   );
