79c92da404f9da0ba8844f3e230a6a0d482c0e5c
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-07-08 12:12:45 +0200

    simplify managetags table

--- services/ssr/src/features/manage-tags/components/TicketTagGroupPreview.js
+++ services/ssr/src/features/manage-tags/components/TicketTagGroupPreview.js
@@ -9,16 +9,12 @@ function TicketTagGroupPreview({ group }) {
       }}
       sx={{ cursor: 'pointer' }}
     >
-      <Grid container sx={{ border: '1px solid gray', padding: '3px' }}>
-        <Grid item xs={2}>
-          <Typography variant="h5" color="pallete.info.dark">
-            {group.name}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Typography variant="h6">{group.description}</Typography>
-        </Grid>
-      </Grid>
+      <Box sx={{ border: '1px solid gray', padding: '3px' }}>
+        <Typography variant="h5" color="pallete.info.dark">
+          {group.name}
+        </Typography>
+        <Typography variant="h6">{group.description}</Typography>
+      </Box>
     </Box>
   );
 }
