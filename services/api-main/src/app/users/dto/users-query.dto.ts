124ac1bd36b0ceb5feb45ee02d1b2dcd5678d816
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-07-22 13:23:01 +0200

    fix: roles default value

--- services/api-main/src/app/users/dto/users-query.dto.ts
+++ services/api-main/src/app/users/dto/users-query.dto.ts
@@ -1,15 +1,12 @@
 import { Transform } from 'class-transformer';
-import { IsArray, IsOptional } from 'class-validator';
 import { EntityQueryDTONew } from 'src/codebase/dto/EntityQueryDTO';
 
 export class UsersQueryDTO extends EntityQueryDTONew {
-  @IsOptional()
-  @IsArray()
   @Transform(({ value }) => {
     if (typeof value === 'string') {
       return value.split(',');
     }
     return value || [];
   })
-  roles: string[];
+  roles: string[] = [];
 }
