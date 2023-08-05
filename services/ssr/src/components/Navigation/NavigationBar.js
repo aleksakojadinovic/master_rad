8aecb00f83ba2b3de23b7bf0e4f02a84f476408b
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-08-05 15:06:28 +0200

    refactor: navigation bar

--- services/ssr/src/components/Navigation/NavigationBar.js
+++ services/ssr/src/components/Navigation/NavigationBar.js
@@ -1,8 +1,10 @@
 import { selectGetMeQueryResponse, useGetMeQuery } from '@/api/auth';
-import { Box, Button } from '@mui/material';
+import { navMessages } from '@/translations/nav';
+import { Box, Button, Menu, MenuItem } from '@mui/material';
 import Cookies from 'js-cookie';
 import dynamic from 'next/dynamic';
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
 
 const AuthenticationModal = dynamic(() =>
@@ -10,65 +12,92 @@ const AuthenticationModal = dynamic(() =>
 );
 
 function NavigationBar() {
+  const intl = useIntl();
   useGetMeQuery();
-  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
-
-  const handleClose = () => setIsAuthModalOpen(false);
-
   const user = useSelector(selectGetMeQueryResponse);
 
-  const renderRightSideContent = () => {
-    if (user != null) {
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+  const [shouldRenderMenu, setShouldRenderMenu] = useState(false);
+  const [menuAnchorRef, setMenuAnchorRef] = useState(null);
+
+  useEffect(() => {
+    setShouldRenderMenu(true);
+  }, []);
+
+  const handleMenuButtonClick = (e) => {
+    setMenuAnchorRef(e.currentTarget);
+  };
+
+  const handleLogout = () => {
+    Cookies.remove('accessToken');
+    window.location.reload();
+  };
+
+  const renderMenuItems = () => {
+    if (user == null) {
       return (
-        <Box>
-          <Button>
-            {user.firstName} {user.lastName}
-          </Button>
-          <Button
-            onClick={() => {
-              Cookies.remove('accessToken');
-              window.location.reload();
-            }}
-          >
-            Logout
-          </Button>
-        </Box>
+        <MenuItem
+          onClick={() => {
+            setIsAuthModalOpen(true);
+            setMenuAnchorRef(null);
+          }}
+        >
+          {intl.formatMessage(navMessages.loginButtonText)}
+        </MenuItem>
       );
     }
-    if (isAuthModalOpen) {
-      return <AuthenticationModal onClose={handleClose} />;
-    }
-    return (
-      <Button
-        onClick={() => setIsAuthModalOpen(true)}
-        onClose={handleClose}
-        disabled={isAuthModalOpen}
-      >
-        Login
-      </Button>
-    );
+    return [
+      <MenuItem key="profile-item">
+        {intl.formatMessage(navMessages.profileButtonText)}
+      </MenuItem>,
+      <MenuItem key="logout-item" onClick={handleLogout}>
+        {intl.formatMessage(navMessages.logoutButtonText)}
+      </MenuItem>,
+    ];
   };
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '40px',
-      }}
-    >
-      <div>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="https://dev.sts.com/images/logo-no-background.svg"
-          alt="Logo"
-          style={{ width: '111px', height: '40px', cursor: 'pointer' }}
-          onClick={() => (window.location.href = '/')}
-        />
-      </div>
-      <div>{renderRightSideContent()}</div>
-    </Box>
+    <Fragment>
+      {isAuthModalOpen && <AuthenticationModal />}
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: '40px',
+        }}
+      >
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://dev.sts.com/images/logo-no-background.svg"
+            alt="Logo"
+            style={{ width: '111px', height: '40px', cursor: 'pointer' }}
+            onClick={() => (window.location.href = '/')}
+          />
+        </div>
+        <div>
+          <Button ref={menuAnchorRef} onClick={handleMenuButtonClick}>
+            {user == null
+              ? intl.formatMessage(navMessages.menuButtonText)
+              : `${user.firstName} ${user.lastName}`}
+          </Button>
+          {shouldRenderMenu && (
+            <Menu
+              id="basic-menu"
+              anchorEl={menuAnchorRef}
+              open={!!menuAnchorRef}
+              onClose={() => setMenuAnchorRef(null)}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              {renderMenuItems()}
+            </Menu>
+          )}
+        </div>
+      </Box>
+    </Fragment>
   );
 }
 
