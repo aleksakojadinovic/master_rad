7a2768b172db4ca5c155ca34f44bd7a6c26c4d6e
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-08-05 18:45:13 +0200

    feat: implement notification menu skeleton

--- services/ssr/src/components/Navigation/NotificationsMenu.js
+++ services/ssr/src/components/Navigation/NotificationsMenu.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import { IconButton, Menu } from '@mui/material';
 
 function NotificationsMenu() {
-  return <CircleNotificationsIcon />;
+  const [menuAnchorRef, setMenuAnchorRef] = useState(null);
+  const [shouldRenderMenu, setShouldRenderMenu] = useState(false);
+
+  useEffect(() => {
+    setShouldRenderMenu(true);
+  }, []);
+
+  const handleMenuButtonClick = (e) => {
+    setMenuAnchorRef(e.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setMenuAnchorRef(null);
+  };
+
+  return (
+    <Fragment>
+      <IconButton onClick={handleMenuButtonClick}>
+        <CircleNotificationsIcon />
+      </IconButton>
+      {shouldRenderMenu && (
+        <Menu
+          anchorEl={menuAnchorRef}
+          onClose={handleCloseMenu}
+          open={!!menuAnchorRef}
+        >
+          test
+        </Menu>
+      )}
+    </Fragment>
+  );
 }
 
 export default NotificationsMenu;
