4d79c90128e7336c206da7dc866359fa2d115ac5
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-08-06 15:42:33 +0200

    chore: eslint

--- services/ssr/src/features/notifications/components/payloads/CommentAddedNotificationPayload.js
+++ services/ssr/src/features/notifications/components/payloads/CommentAddedNotificationPayload.js
@@ -1,7 +1,6 @@
 import { Box } from '@mui/material';
 import React from 'react';
 
-import UserChip from '@/components/User/UserChip';
 import TicketTitlePreviewAtom from '../atoms/TicketTitlePreviewAtom';
 import CommentPreviewAtom from '../atoms/CommentPreviewAtom';
 import UserPreviewAtom from '../atoms/UserPreviewAtom';
