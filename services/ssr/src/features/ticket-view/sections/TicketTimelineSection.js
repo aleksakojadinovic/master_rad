ef4ae9577a16caabefe4a972ff208b6761c7636f
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-09-14 21:01:09 +0200

    chore: cleanup

--- services/ssr/src/features/ticket-view/sections/TicketTimelineSection.js
+++ services/ssr/src/features/ticket-view/sections/TicketTimelineSection.js
@@ -1,7 +1,4 @@
-import Comment from '@/features/ticket-view/components/changes/atoms/Comment/Comment';
-import StatusChange from '@/features/ticket-view/components/changes/atoms/StatusChange';
-import { TicketHistoryEntryType } from '@/enums/tickets';
-import { Box, Card, CardContent } from '@mui/material';
+import { Box } from '@mui/material';
 import React, { useMemo } from 'react';
 import { TICKET_HISTORY_ENTRY_TYPE } from '../constants';
 import TicketChange from '../components/changes/TicketChange';
@@ -31,57 +28,6 @@ function TicketTimelineSection({ ticket }) {
       <TicketChange item={item} />
     </Box>
   ));
-  // const wrap = (content, index) => (
-  //   <Box key={index} marginTop="12px">
-  //     {content}
-  //   </Box>
-  // );
-
-  // let previousStatus = null;
-
-  // return ticket.history.map((item, index) => {
-  //   switch (item.type) {
-  //     case TicketHistoryEntryType.COMMENT_ADDED:
-  //       return wrap(
-  //         <Comment
-  //           comment={{
-  //             ...item.payload,
-  //             user: item.user,
-  //             timestamp: item.timestamp,
-  //           }}
-  //         />,
-  //         index,
-  //       );
-
-  //     case TicketHistoryEntryType.STATUS_CHANGED: {
-  //       const status = item.payload.status;
-  //       const prevStatus = previousStatus;
-  //       previousStatus = status;
-  //       return wrap(
-  //         <Box
-  //           display="flex"
-  //           justifyContent="center"
-  //           marginTop="20px"
-  //           marginBottom="20px"
-  //         >
-  //           <Card>
-  //             <CardContent>
-  //               <StatusChange
-  //                 statusChange={{
-  //                   user: item.user,
-  //                   timestamp: item.timestamp,
-  //                   statusFrom: prevStatus,
-  //                   statusTo: status,
-  //                 }}
-  //               />
-  //             </CardContent>
-  //           </Card>
-  //         </Box>,
-  //         index,
-  //       );
-  //     }
-  //   }
-  // });
 }
 
 export default TicketTimelineSection;
