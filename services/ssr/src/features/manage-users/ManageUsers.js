b5482dee1cfb88b59e98a2587b7a39b809e361ff
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-09-16 13:44:07 +0200

    feat: move userfilters into a component

--- services/ssr/src/features/manage-users/ManageUsers.js
+++ services/ssr/src/features/manage-users/ManageUsers.js
@@ -4,11 +4,8 @@ import Head from 'next/head';
 import React, { Fragment } from 'react';
 import { useIntl } from 'react-intl';
 import UsersTable from './components/UsersTable';
-import { Box } from '@mui/material';
 import { useRouter } from 'next/router';
-import RoleFilter from './components/RoleFilter';
-import SearchBox from './components/SearchBox';
-import StatusFilter from './components/StatusFilter';
+import UserFilters from './components/UserFilters';
 
 function ManageUsers({ queryParams }) {
   const intl = useIntl();
@@ -38,61 +35,20 @@ function ManageUsers({ queryParams }) {
     handleParamsChange(newQueryParams);
   };
 
-  const handleRoleChange = (role) => {
-    const newQueryParams = {
-      ...queryParams,
-      roles: role,
-      page: 1,
-    };
-    handleParamsChange(newQueryParams);
-  };
-
-  const handleSearchChange = (search) => {
-    const resolvedSearchValue = search.trim() || null;
-    const newQueryParams = {
-      ...queryParams,
-      searchString: resolvedSearchValue,
-      page: 1,
-    };
-
-    handleParamsChange(newQueryParams);
-  };
-
-  const handleStatusChange = (status) => {
-    const newQueryParams = {
-      ...queryParams,
-      statuses: status,
-      page: 1,
-    };
-    handleParamsChange(newQueryParams);
-  };
-
   return (
     <Fragment>
       <Head>
         <title>{intl.formatMessage(manageUsersMessages.pageTitle)}</title>
       </Head>
 
-      <Box marginBottom="12px" display="flex" flexWrap="wrap" gap="12px">
-        <SearchBox
-          value={queryParams.searchString}
-          onChange={handleSearchChange}
+      <UserFilters value={queryParams} onChange={handleParamsChange} />
+      {isSuccess && (
+        <UsersTable
+          data={data}
+          onPageChange={handlePageChange}
+          onPerPageChange={handlePerPageChange}
         />
-        <RoleFilter value={queryParams.roles} onChange={handleRoleChange} />
-        <StatusFilter
-          value={queryParams.statuses}
-          onChange={handleStatusChange}
-        />
-      </Box>
-      <Box>
-        {isSuccess && (
-          <UsersTable
-            data={data}
-            onPageChange={handlePageChange}
-            onPerPageChange={handlePerPageChange}
-          />
-        )}
-      </Box>
+      )}
     </Fragment>
   );
 }
