ddd3a6ba483d177269daa537b193892b5e5e5f7a
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-09-17 21:07:32 +0200

    feat: metrics display

---
+++ services/analytics/api.py
@@ -0,0 +1,53 @@
+import os
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import streamlit as st
+import math
+
+MONGODB_USERNAME = os.getenv("MAIN_DB_USERNAME")
+MONGODB_PASSWORD = os.getenv("MAIN_DB_PWD")
+
+client = MongoClient(
+    host='mongodb://maindb',
+    port=27017,
+    username=MONGODB_USERNAME,
+    password=MONGODB_PASSWORD
+)
+db = client["sts_analytics_db"]  # Replace with your database name
+tickets = db["tickets"]  # Replace with your collection name
+
+DEFAULT_END = datetime.now()
+DEFAULT_START = (datetime.now() - timedelta(days=7))
+
+
+def get_average_metric(start, end, metric):
+    pipeline = [
+        {
+            "$match": {
+                "timestamp": {"$gte": start, "$lte": end},
+                f"{metric}": {"$ne": None}
+
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_id",
+                "average_result": {"$avg": f"${metric}"}
+            }
+        }
+    ]
+    result = list(tickets.aggregate(pipeline))
+    average_result = result[0]["average_result"]
+    return round(average_result / 60, 2)
+
+
+def calculate_average_resolution_time(start=DEFAULT_START, end=DEFAULT_END):
+    return get_average_metric(start, end, 'resolution_time')
+
+
+def calculate_average_pickup_time(start=DEFAULT_START, end=DEFAULT_END):
+    return get_average_metric(start, end, 'pickup_time')
+
+
+def calculate_average_first_response_time(start=DEFAULT_START, end=DEFAULT_END):
+    return get_average_metric(start, end, 'first_response_time')
--- services/analytics/app.py
+++ services/analytics/app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+from datetime import datetime
 
+from api import calculate_average_first_response_time, calculate_average_pickup_time, calculate_average_resolution_time
+
+
+st.set_page_config(layout="wide")
 st.title('STS analytics dashboard')
+
+average_resolution_time = calculate_average_resolution_time()
+average_pickup_time = calculate_average_pickup_time()
+average_first_response_time = calculate_average_first_response_time()
+
+d_from = st.date_input("From")
+d_to = st.date_input("To")
+
+
+st.metric(label="Average resolution time",
+          value=f"{average_resolution_time} h")
+
+st.metric(label="Average pickup time",
+          value=f"{average_pickup_time} h")
+
+st.metric(label="Average first response time",
+          value=f"{average_first_response_time} h")
