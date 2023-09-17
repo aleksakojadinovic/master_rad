67762b41440bce05ec521401ea3ae193b7551ada
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-09-17 13:06:34 +0200

    feat: add <pre> to comment body

--- services/ssr/src/features/ticket-view/components/changes/atoms/Comment/CommentBody/CommentBodyRegular.js
+++ services/ssr/src/features/ticket-view/components/changes/atoms/Comment/CommentBody/CommentBodyRegular.js
@@ -2,7 +2,11 @@ import { Typography } from '@mui/material';
 import React from 'react';
 
 function CommentBodyRegular({ comment }) {
-  return <Typography variant="body2">{comment.body}</Typography>;
+  return (
+    <Typography variant="body2" as="div">
+      <pre>{comment.body}</pre>
+    </Typography>
+  );
 }
 
 export default CommentBodyRegular;
