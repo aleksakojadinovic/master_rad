640c19f551147d43354794e07623072e5a082800
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-09-24 19:11:28 +0200

    cleanup: remove unused fields in updateticketdto

--- services/api-main/src/app/tickets/api/dto/update-ticket.dto.ts
+++ services/api-main/src/app/tickets/api/dto/update-ticket.dto.ts
@@ -1,19 +1,7 @@
-import { IsArray, IsOptional, Validate } from 'class-validator';
-import { isValidObjectId } from 'mongoose';
 import { TicketStatus } from '../../domain/value-objects/ticket-status';
 
 export class UpdateTicketDto {
   status?: TicketStatus;
   body?: string;
   title?: string;
-
-  @Validate(isValidObjectId, { each: true })
-  @IsOptional()
-  @IsArray()
-  addAssignees?: string[];
-
-  @Validate(isValidObjectId, { each: true })
-  @IsOptional()
-  @IsArray()
-  removeAssignees?: string[];
 }
