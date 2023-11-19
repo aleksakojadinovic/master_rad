13feeb7e54a1681ca9c0bef434e63f3d04545910
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-11-20 00:04:59 +0100

    feat: edit title fe

--- services/ssr/src/features/ticket-view/sections/TicketTitleSection.js
+++ services/ssr/src/features/ticket-view/sections/TicketTitleSection.js
@@ -1,40 +1,106 @@
 import { formatDate } from '@/utils';
-import { Box, Grid, Typography } from '@mui/material';
-import React from 'react';
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React, { Fragment, useEffect, useState } from 'react';
 import TicketStatusBadge from '../components/TicketStatusBadge';
 import { useIntl } from 'react-intl';
 import { globalMessages } from '@/translations/global';
 import useUser from '@/hooks/useUser';
+import { useEditTitleMutation } from '@/api/tickets';
+import EditIcon from '@mui/icons-material/Edit';
 
 function TicketTitleSection({ ticket }) {
   const intl = useIntl();
-
   const { id } = useUser();
   const isOwner = id === ticket.createdBy.id;
+
+  const [title, setTitle] = useState(null);
+  const isEditing = title !== null;
+
+  const [editTitle, { isSuccess }] = useEditTitleMutation();
+
+  const handleSubmitEdit = () => {
+    editTitle({ id: ticket.id, title });
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTitle(null);
+    }
+  }, [isSuccess]);
+
+  const isTitleValid = title?.length >= 20;
 
   return (
     <Grid container>
       <Grid item xs={12} md={9}>
-        <Box display="flex" alignItems="center" height="100%">
-          <Typography variant="h4">{ticket.title}</Typography>
-        </Box>
-      </Grid>
-      <Grid item xs={12} md={3}>
         <Box
           display="flex"
-          flexDirection="column"
-          justifyContent="center"
+          alignItems="center"
           height="100%"
+          gap="12px"
+          flexWrap="wrap"
         >
-          <Typography component="div" sx={{ color: 'text.disabled' }}>
-            {formatDate(ticket.createdAt, intl)}
-          </Typography>
-          <Typography component="div" sx={{ color: 'secondary.main' }}>
-            {intl.formatMessage(globalMessages.by)} {ticket.createdBy.fullName}
-          </Typography>
-          <TicketStatusBadge status={ticket.status} />
+          {!isEditing && (
+            <Fragment>
+              <Typography variant="h4">
+                {ticket.title}
+                <IconButton onClick={() => setTitle(ticket.title)}>
+                  <EditIcon />
+                </IconButton>
+              </Typography>
+            </Fragment>
+          )}
+
+          {isEditing && (
+            <TextField
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              fullWidth
+            />
+          )}
+
+          {isEditing && (
+            <Box display="flex">
+              <Button
+                variant="contained"
+                onClick={handleSubmitEdit}
+                disabled={!isTitleValid || title === ticket.title}
+              >
+                {intl.formatMessage(globalMessages.save)}
+              </Button>
+              <Button color="error" onClick={() => setTitle(null)}>
+                {intl.formatMessage(globalMessages.discard)}
+              </Button>
+            </Box>
+          )}
         </Box>
       </Grid>
+      {!isEditing && (
+        <Grid item xs={12} md={3}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            height="100%"
+          >
+            <Typography component="div" sx={{ color: 'text.disabled' }}>
+              {formatDate(ticket.createdAt, intl)}
+            </Typography>
+            <Typography component="div" sx={{ color: 'secondary.main' }}>
+              {intl.formatMessage(globalMessages.by)}{' '}
+              {ticket.createdBy.fullName}
+            </Typography>
+            <TicketStatusBadge status={ticket.status} />
+          </Box>
+        </Grid>
+      )}
     </Grid>
   );
 }
