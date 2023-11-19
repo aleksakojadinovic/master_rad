5d387beae15cc0ec332e618953c278846142bf45
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2023-11-19 20:38:37 +0100

    feat: add fancy 404 page

--- services/ssr/src/pages/404.js
+++ services/ssr/src/pages/404.js
@@ -1,4 +1,6 @@
 import { globalMessages } from '@/translations/global';
+import { notFoundMessages } from '@/translations/not-found-page';
+import { Alert, Box, Typography } from '@mui/material';
 import Head from 'next/head';
 import React, { Fragment } from 'react';
 import { useIntl } from 'react-intl';
@@ -10,7 +12,23 @@ function NotFound() {
       <Head>
         <title>{intl.formatMessage(globalMessages.notFound)}</title>
       </Head>
-      <div>NotFound</div>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Typography variant="h3">
+          {intl.formatMessage(notFoundMessages.header)}
+        </Typography>
+        <Box marginTop="20px">
+          <Alert severity="error">
+            <Typography variant="body1">
+              {intl.formatMessage(notFoundMessages.description)}
+            </Typography>
+          </Alert>
+        </Box>
+      </Box>
     </Fragment>
   );
 }
