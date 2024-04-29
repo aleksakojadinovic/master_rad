4625458e28d05d6b7ef0e06c89bede63b315f5af
Aleksa Kojadinovic <aleksa.kojadinovic@fishingbooker.com>
2024-04-29 13:45:49 +0200

    fix(manage-users): add translations for actions

--- services/ssr/src/features/manage-users/components/actions/atoms/UserActionOptions.js
+++ services/ssr/src/features/manage-users/components/actions/atoms/UserActionOptions.js
@@ -2,8 +2,12 @@ import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import { useIntl } from 'react-intl';
+import { manageUsersMessages } from '@/translations/manage-users';
 
 function UserActionOptions({ onChangeRoleClick, onChangeStatusClick }) {
+  const intl = useIntl();
+
   return (
     <Box display="flex" flexDirection="column" gap="3px">
       <Button
@@ -12,7 +16,9 @@ function UserActionOptions({ onChangeRoleClick, onChangeStatusClick }) {
         variant="outlined"
         size="small"
       >
-        <Typography variant="caption">Change status</Typography>
+        <Typography variant="caption">
+          {intl.formatMessage(manageUsersMessages.changeStatusTitle)}
+        </Typography>
       </Button>
       <Button
         onClick={onChangeRoleClick}
@@ -21,7 +27,9 @@ function UserActionOptions({ onChangeRoleClick, onChangeStatusClick }) {
         size="small"
         color="warning"
       >
-        <Typography variant="caption">Change role</Typography>
+        <Typography variant="caption">
+          {intl.formatMessage(manageUsersMessages.changeRoleTitle)}
+        </Typography>
       </Button>
     </Box>
   );
